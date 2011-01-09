2f284c7aa16573fe20332a5ea60a2065389bf13d
Nikolay Petrachkov <nik@jastix.me>
2011-01-09 16:55:24 +0100

    users non confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable#, :validatable
+  devise :authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable#, :validatable, :confirmable,
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :trained
